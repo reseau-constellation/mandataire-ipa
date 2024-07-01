343fbc8476e36369ebb0b8cd6bd4ab85b5d9c60a
julienmalard <julien.malard@mail.mcgill.ca>
2024-07-01 13:22:24 +0200

    Exporter types

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,16 @@ export {
   Mandatairifiable,
   générerMandataire,
 } from "@/mandataire.js";
+export type {
+  MessageDIpa,
+  MessageActionDIpa,
+  MessageErreurDIpa,
+  MessageSuivreDIpa,
+  MessageSuivrePrêtDIpa,
+  MessagePourIpa,
+  MessageSuivrePourIpa,
+  MessageActionPourIpa,
+  MessageRetourPourIpa,
+} from "@/messages.js"
 
 export { version } from "@/version.js";
